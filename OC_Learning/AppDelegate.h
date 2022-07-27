01d8cebd3c8c5a0f1ec0565934cc1143d61fcd2e
lsaac <2423860442@qq.com>
2022-07-27 23:10:11 +0800

    Initial Commit

---
+++ OC_Learning/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OC_Learning
+//
+//  Created by lsaac on 2022/7/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
