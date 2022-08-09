391569ae38ddb70190c260eddd90ff9061195ca4
lsaac <2423860442@qq.com>
2022-08-09 17:18:05 +0800

    线程生命周期

--- OC_Learning/thread/NSThreadVC.m
+++ OC_Learning/thread/NSThreadVC.m
@@ -12,7 +12,18 @@
 @end
 
 @implementation NSThreadVC
-
+//线程的生命周期:当线程中的任务执行完毕之后结束
+//线程状态
+// alloc init新建线程对象存储在内存中（新建状态）
+//start 将线程放入可调度线程池中（就绪状态）
+// -(void) start;
+//当CPU调度当前线程时，线程处于(运行状态)
+//当CPU调度其他线程时，线程处于(就绪状态)
+//当调用sleep方法/等待同步锁时处于(阻塞状态),会被移除课调度线程池，但是还在内存中
+// +(void) sleepUntilDate:(NSDate *)date;
+// +(void) sleepForTiemInterval :(NSDate *)date;
+//当线程任务执行完毕，异常/强制退出 ，线程就处于(死亡状态),会被移除课调度线程池，并从内存释放掉
+//exit;
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
@@ -43,7 +54,7 @@
     thread.name = @"nsthreadOne";
     //优先级高的先执行(取值范围0-1)默认0.5
     thread.threadPriority = 1;
-    //启动线程
+    //启动线程  进入就绪状态。当线程任务执行完毕，自动进入死亡状态
     [thread start];
     
     //创建线程的方式2，分离一个线程出来,并自动启动,缺点：无法获取线程对象
