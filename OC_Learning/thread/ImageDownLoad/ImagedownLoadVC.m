f9884caa94c2a104b68c66e3a4d0dc93acbcf886
lsaac <2423860442@qq.com>
2022-08-11 00:48:26 +0800

    解决因为使用多线程导致的数据错乱，重复下载等问题

--- OC_Learning/thread/ImageDownLoad/ImagedownLoadVC.m
+++ OC_Learning/thread/ImageDownLoad/ImagedownLoadVC.m
@@ -11,15 +11,32 @@
 @interface ImagedownLoadVC ()
 @property(nonatomic,strong)NSArray *apps;
 @property(nonatomic,strong)NSMutableDictionary *images;
+@property(nonatomic,strong)NSOperationQueue *queue;
+@property(nonatomic,strong)NSMutableDictionary *operations;
 @end
 
 @implementation ImagedownLoadVC
+- (NSOperationQueue *)queue
+{
+    if (!_queue) {
+        _queue = [[NSOperationQueue alloc] init];
+        _queue.maxConcurrentOperationCount = 5;
+    }
+    return _queue;
+}
 - (NSMutableDictionary *)images
 {
     if (!_images) {
         _images = [NSMutableDictionary dictionary];
     }
     return _images;
+}
+- (NSMutableDictionary *)operations
+{
+    if (!_operations) {
+        _operations = [NSMutableDictionary dictionary];
+    }
+    return _operations;
 }
 ///懒加载
 - (NSArray *)apps
@@ -78,6 +95,9 @@
     //存在问题
     //1.UI不流畅->开子线程
     //2.图片被重复下载 ->先把之前已经下载的图片保存起来用字典
+    //3.图片不会刷新 reload
+    //4.重复下载(手速过快，图片下载需要时间，图片还没下载完，就滑动又开启线程下载)
+    //5.图片错乱：重用导致
 
     //先查看内存缓存中十分存在，不存在再查看磁盘缓存没有再下载
     UIImage *img = [self.images objectForKey:app.icon];
@@ -95,20 +115,54 @@
         if(imageData){
             UIImage *image = [UIImage imageWithData:imageData];
             cell.imageView.image = image;
+            NSLog(@"使用磁盘图片");
             //把图片进行内存缓存
            [self.images setObject:image forKey:app.icon];
         }else{
-            NSURL *url = [NSURL URLWithString:app.icon];
-            NSData *imageData = [NSData dataWithContentsOfURL:url];
-            UIImage *image = [UIImage imageWithData:imageData];
-            cell.imageView.image = image;
-            NSLog(@"下载新图片");
-            //把图片进行内存缓存
-           [self.images setObject:image forKey:app.icon];
-            //把图片保存到Caches路径
+    
+            //检查该图片是否正在下载，如果是就不做操作，否则添加下载任务
+            NSOperation *isDownLoading = [self.operations objectForKey:app.icon];
+            if (isDownLoading) {
+                
+            }else{
+                //解决重用导致的显示错乱
+//                cell.imageView.image = nil; 显示nil影响用户体验，所以给个展位图
+                cell.imageView.image = [UIImage imageNamed:@"people"];
+                NSBlockOperation *download = [NSBlockOperation blockOperationWithBlock:^{
+                    NSURL *url = [NSURL URLWithString:app.icon];
+                    NSData *imageData = [NSData dataWithContentsOfURL:url];
+                    
+                    //模拟网速慢
+                    [NSThread sleepForTimeInterval:2];
+                    
+                    UIImage *image = [UIImage imageWithData:imageData];
+                    //线程间通信
+                    [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                                        
+                        [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationLeft];
+                        cell.imageView.image = image;
+                                    }];
+                   
+                    NSLog(@"下载新图片");
+                    //把图片进行内存缓存
+                   [self.images setObject:image forKey:app.icon];
+                    //把图片保存到Caches路径
+                   
+                    //写数据到磁盘
+                    [imageData writeToFile:fullPath atomically:YES];
+                    
+                    //移除图片的下载操作
+                    [self.operations removeObjectForKey:app.icon];
+                }];
+                
+                //添加操作到操作缓存中
+                [self.operations setObject:download forKey:app.icon];
+                //添加操作到队列中
+                [self.queue addOperation:download];
+            }
+            
            
-            //写数据到磁盘
-            [imageData writeToFile:fullPath atomically:YES];
+           
         }
        
     }
