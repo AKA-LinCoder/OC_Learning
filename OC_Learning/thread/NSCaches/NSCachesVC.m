1f2d2550c43eadf02b05391960c30f88b2f1bf76
lsaac <2423860442@qq.com>
2022-08-12 10:18:33 +0800

    位移枚举

--- OC_Learning/thread/NSCaches/NSCachesVC.m
+++ OC_Learning/thread/NSCaches/NSCachesVC.m
@@ -6,6 +6,29 @@
 //
 
 #import "NSCachesVC.h"
+//第一种枚举
+typedef enum {
+    DemoTypeTop,
+    DemoTypeBottom,
+}DemoType;
+
+//第二种枚举，第一个参数为类型，第二个参数为名称
+typedef NS_ENUM(NSInteger,LinType){
+    boy,
+    gril,
+};
+//第三种，位移枚举
+//一个参数可以传多个值
+//如果是位移枚举，观察第一个枚举值，如果该枚举值！=0，那么可以默认传0做参数，如果传0这样效率最高
+typedef NS_OPTIONS(NSInteger, LinAction){
+    linPlay = 1<<0, //1 1*(2)^0
+    linStudy = 1<<1, //2 1*(2)^1
+    linEat = 1<<2, //4 1*(2)^2
+    linSleep = 1<<3, //8 1*(2)^3
+};
+//按位与 & 1&1==1 1&0==0， 0&0==0 只要有0就是0
+//按位或 ｜ 1｜1==1 1｜0==1 0｜0==0 只要有1就是1
+
 
 @interface NSCachesVC ()<NSCacheDelegate>
 
@@ -13,6 +36,21 @@
 @end
 
 @implementation NSCachesVC
+
+/// 位移枚举例子
+/// @param action 传人的枚举
+-(void)demo:(LinAction) action
+{
+    if(action & linPlay)
+    {
+        NSLog(@"玩耍---%zd",action & linPlay);
+    }
+    if(action & linEat)
+    {
+        NSLog(@"干饭---%zd",action & linEat);
+    }
+    
+}
 
 - (NSCache *)cache
 {
@@ -26,6 +64,7 @@
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self demo:linPlay|linEat|linSleep];
     // Do any additional setup after loading the view from its nib.
 }
 
