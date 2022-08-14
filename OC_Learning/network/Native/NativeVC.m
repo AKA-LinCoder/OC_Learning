847042e3e07534b7eeed947b7de6187473e376db
lsaac <2423860442@qq.com>
2022-08-14 14:47:06 +0800

    使用NSURLConnection的delegate发送get请求

--- OC_Learning/network/Native/NativeVC.m
+++ OC_Learning/network/Native/NativeVC.m
@@ -7,8 +7,8 @@
 
 #import "NativeVC.h"
 
-@interface NativeVC ()
-
+@interface NativeVC ()<NSURLConnectionDataDelegate>
+@property(nonatomic,strong)NSMutableData *resultData;
 @end
 
 @implementation NativeVC
@@ -16,9 +16,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor orangeColor];
-    [self sendAsyncGet];
+    [self delegate];
 }
-
+- (NSMutableData *)resultData
+{
+    if (!_resultData) {
+        _resultData = [NSMutableData data];
+    }
+    return _resultData;
+}
 /*
  请求：请求头(NSURLRequest默认包含)+请求体(Get没有)
  响应:响应头(真实类型->NSHTTPURLResponse)
@@ -72,5 +78,47 @@
         NSLog(@"请求结果2:%@",dataString);
     }];
 }
+-(void)delegate
+{
+    //1.确定请求路径
+    NSURL *url = [NSURL URLWithString:@"http://192.168.0.148:8080/haha/app/workBench/getList"];
+    //2.创建请求对象
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    
+    //3.1设置代理发送网络请求
+//    [NSURLConnection connectionWithRequest:request delegate:self];
+    //3.2，设置代理，并不会发送请求，只有第三个参数为YES，或者手动发送
+    NSURLConnection *connect = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:NO ];
+    [connect start];
+    [connect cancel];
+    
+}
+#pragma mark -NSURLConnectionDataDelegate
+//1.当接受到服务器响应的时候调用
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    NSLog(@"%s",__func__);
+}
+//2.节后到服务器返回数据的时候调用,可能会调用多次
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    NSLog(@"%s",__func__);
+    //拼接数据
+    [self.resultData appendData:data];
+}
+//3.当请求失败的时候
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    NSLog(@"%s",__func__);
+}
 
+
+//4.请求结束的时候调用
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    NSLog(@"%s",__func__);
+    //解析数据
+    NSString *dataString = [[NSString alloc] initWithData:self.resultData encoding:NSUTF8StringEncoding];
+    NSLog(@"请求结果3:%@",dataString);
+}
 @end
