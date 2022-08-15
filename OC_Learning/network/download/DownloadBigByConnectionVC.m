c93ab51c614d10e7a332afad0b9a196d602e86d3
lsaac <2423860442@qq.com>
2022-08-15 16:27:35 +0800

    使用NSURLConnection进行断点续传

--- OC_Learning/network/download/DownloadBigByConnectionVC.m
+++ OC_Learning/network/download/DownloadBigByConnectionVC.m
@@ -100,7 +100,11 @@
 -(void) downLoad
 {
     NSURL *url = [NSURL URLWithString:@"https://vd4.bdstatic.com/mda-mc9ef5ja85iv0chp/sc/cae_h264_nowatermark/1615343513/mda-mc9ef5ja85iv0chp.mp4?v_from_s=hkapp-haokan-hna&auth_key=1660533265-0-0-63527dcc58b9cdb7b1a221b55b338f9f&bcevod_channel=searchbox_feed&pd=1&cd=0&pt=3&logid=2664869369&vid=12087023396548225925&abtest=103525_1-103579_2-103890_2&klogid=2664869369"];
-    NSURLRequest *request= [NSURLRequest requestWithURL:url];
+    NSMutableURLRequest *request= [NSMutableURLRequest requestWithURL:url];
+    //断点下载
+    //设置请求头
+    NSString *range = [NSString stringWithFormat:@"bytes=%zd-",self.currentSize];
+    [request setValue:range forHTTPHeaderField:@"Range"];
     self.connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
 }
 #pragma mark - delegate
@@ -120,6 +124,12 @@
 }
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
 {
+    //如果写在前面 本次请求的数据大小！=文件总大小
+//    self.videoSize = response.expectedContentLength + self.currentSize;
+    if (self.currentSize) {
+        return;
+    }
+    
     //1.得到文件的总大小(本次请求的文件数据的总大小)
     self.videoSize = response.expectedContentLength;
     //2.写数据到沙盒
@@ -140,9 +150,6 @@
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
     NSLog(@"finished");
-
     NSLog(@"%@",self.fullPath);
-//    [self.data writeToFile:fullPath atomically:YES];
-    
 }
 @end
