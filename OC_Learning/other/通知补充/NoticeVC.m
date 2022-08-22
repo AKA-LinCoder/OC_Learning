7e33b17d3dd9aba5527a978485fab5108498058f
lsaac <2423860442@qq.com>
2022-08-22 16:19:42 +0800

    接受通知的执行方法所在的线程由发送通知的方法所在的线程决定

--- OC_Learning/other/通知补充/NoticeVC.m
+++ OC_Learning/other/通知补充/NoticeVC.m
@@ -25,33 +25,68 @@
     
     
     //监听通知
-    //方式一
-//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reciveNotic) name:@"note" object:nil];
-    //方式二，因为没有观察者
-    observe =  [[NSNotificationCenter defaultCenter] addObserverForName:@"note" object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
-        //只要监听到通知就会调用
-        //queue：决定block发生哪个线程调用，如果传nil，就和发送通知的线程保持一样
-            NSLog(@"监听通知回调");
-        }];
-    //发送通知
-    //name:通知名称
-    //object:谁发送的通知
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"note" object:nil];
+    [self listenNoticeOneAsync];
+
+    
     
     
     //先发送，再监听没有效果，必须先监听再发送
 }
+
+-(void) listenNoticeOne
+{
+    //方式一
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reciveNotic) name:@"note" object:nil];
+    
+
+}
+-(void) listenNoticeTwo
+{
+    //方式二，一般使用主队列，避免block中有更新UI的操作
+    self.observe =  [[NSNotificationCenter defaultCenter] addObserverForName:@"note" object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        //只要监听到通知就会调用
+        //queue：决定block发生哪个线程调用，如果传nil，就和发送通知的线程保持一样
+            NSLog(@"监听通知回调");
+        }];
+    
+
+}
+
+-(void) listenNoticeOneAsync
+{
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        //方式一
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reciveNotic) name:@"note" object:nil];
+    });
+    
+
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //发送通知
+    //name:通知名称
+    //object:谁发送的通知
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"note" object:nil];
+}
+
 //一个对象即将销毁就会调用
 - (void)dealloc
 {
-//    [[NSNotificationCenter defaultCenter] removeObserver:self] ;
+    [[NSNotificationCenter defaultCenter] removeObserver:self] ;
     //方式二的注销
-    [[NSNotificationCenter defaultCenter] removeObserver:self.observe];
+//    [[NSNotificationCenter defaultCenter] removeObserver:self.observe];
 }
 //监听到通知就会调用
 -(void) reciveNotic
 {
+    //注意点：接受通知的代码，由发出通知的线程决定
+    NSLog(@"%@",[NSThread currentThread]);
     NSLog(@"%s",__func__);
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        //更新UI
+    });
 }
 
 
