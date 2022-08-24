d452691488a9bcd633d3688d08a1dd28b0d6f317
lsaac <2423860442@qq.com>
2022-08-24 09:03:43 +0800

    block在MRC中的内存管理

--- OC_Learning/other/block/BlockVC.m
+++ OC_Learning/other/block/BlockVC.m
@@ -71,4 +71,30 @@ typedef  void(^BlockType)();
 {
     NSLog(@"这是获取到的值%@",something);
 }
+
+/// 内存管理
+/// @param sender 对象
+- (IBAction)arcAndmrc:(id)sender {
+    //block是OC对象,可以添加到集合中
+    /*
+     判断当前文件是MRC还是ARC
+     1.dealloc 能否调用super，只有MRC才能调用super
+     2.能否使用retain，release，如果能用就是MRC
+     3.
+     */
+    /*
+     MRC：没有strong，weak，局部变量对象就是相当于基本数据类型
+            M RC给成员变量赋值，一定要使用set方法，不能直接访问下划线成员属性赋值
+     ARC:只要一个对象没有被强指针修饰就会被销毁，默认局部变量对象都是强指针
+     */
+    
+    //MRC
+    //只要block引用了外部局部变量，block就放在栈里（因为MRC没有强引用的概念，都可以算是基本数据类型）；只要block没有引用外部局部变量，block就放还在全局区；block只能使用copy，不能使用retain，使用retain，block还是在栈里，代码块一过就销毁，可能导致坏的访问；使用copy就会放在堆里；
+    int a = 3;
+    int(^block3)() = ^int{
+        NSLog(@"%d",a);
+        return 3;
+    };
+    NSLog(@"%@",block3);
+}
 @end
