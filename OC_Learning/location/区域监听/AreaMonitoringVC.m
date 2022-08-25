3cee6b280056905ad1e126756569dd06730111a9
lsaac <2423860442@qq.com>
2022-08-25 19:02:14 +0800

    对区域监听进行约束

--- OC_Learning/location/区域监听/AreaMonitoringVC.m
+++ OC_Learning/location/区域监听/AreaMonitoringVC.m
@@ -8,6 +8,7 @@
 #import "AreaMonitoringVC.h"
 #import "CoreLocation/CoreLocation.h"
 @interface AreaMonitoringVC ()<CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *noticeLabel;
 @property(nonatomic,strong)CLLocationManager *manager;
 @end
 
@@ -25,20 +26,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(21.123, 123.354);
-    CLLocationDistance distance = 1000;
-    CLCircularRegion *regin = [[CLCircularRegion alloc ] initWithCenter:center radius: distance identifier:@"lin"];
     
-    [self.manager startMonitoringForRegion:regin];
+    //判断当前区域是否可以监听
+    if ([CLLocationManager isMonitoringAvailableForClass:NSClassFromString(@"CLCircularRegion")]) {
+        CLLocationCoordinate2D center = CLLocationCoordinate2DMake(21.123, 123.354);
+        CLLocationDistance distance = 1000;
+        if (distance>self.manager.maximumRegionMonitoringDistance) {
+            distance = self.manager.maximumRegionMonitoringDistance;
+        }
+        CLCircularRegion *regin = [[CLCircularRegion alloc ] initWithCenter:center radius: distance identifier:@"lin"];
+        
+        [self.manager startMonitoringForRegion:regin];
+        
+        //请求某个区域的状态
+        [self.manager requestStateForRegion:regin];
+    }
+    
+    
+  
 }
 
 #pragma mark-CLLocationManagerDelegate
 - (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
 {
     NSLog(@"进入区域");
+    self.noticeLabel.text = @"进入区域";
 }
 - (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
 {
     NSLog(@"离开区域");
+    self.noticeLabel.text = @"离开区域";
+}
+- (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region
+{
+    if([region.identifier isEqual: @"lin"]){
+        if (state == CLRegionStateInside) {
+            self.noticeLabel.text = @"在里面";
+        }else if (state == CLRegionStateOutside){
+            self.noticeLabel.text = @"在外面";
+        }else{
+            self.noticeLabel.text = @"未知";
+        }
+    }
 }
 @end
