d9622bd095095c59e62e4a2bb9020c8475fc5f2d
lsaac <2423860442@qq.com>
2022-09-09 10:11:45 +0800

    拖动缩放页面内容

--- OC_Learning/ui/SelfPage/SelfPageVC.m
+++ OC_Learning/ui/SelfPage/SelfPageVC.m
@@ -6,16 +6,42 @@
 //
 
 #import "SelfPageVC.h"
-
+//#define imageHeight 200;
 @interface SelfPageVC ()<UITableViewDelegate,UITableViewDataSource>
 @property(nonatomic,strong)UITableView *tableView;
+@property(nonatomic,strong)UIImageView *headerView;
+@property(nonatomic,strong)UIView *toolView;
+@property(nonatomic,assign) CGFloat offet;
 @end
-
+ CGFloat imageHeight = 200;
 @implementation SelfPageVC
+- (UIView *)headerView
+{
+    if (_headerView == nil) {
+        _headerView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, imageHeight)];
+        _headerView.backgroundColor = [UIColor redColor];
+        [_headerView setClipsToBounds:YES];
+//        _headerView.contentMode = UIViewContentModeScaleAspectFit;
+//        UIImageView *imageView = [[UIImageView alloc] initWithFrame:_headerView.frame];
+        _headerView.image = [UIImage imageNamed:@"people"];
+//        [_headerView addSubview:imageView];
+        
+    }
+    return  _headerView;
+}
+- (UIView *)toolView
+{
+    if (_toolView == nil) {
+        _toolView = [[UIView alloc] initWithFrame:CGRectMake(0, imageHeight, [UIScreen mainScreen].bounds.size.width, 30)];
+        _toolView.backgroundColor = [UIColor orangeColor];
+       
+    }
+    return  _toolView;
+}
 - (UITableView *)tableView
 {
     if(_tableView == nil){
-        _tableView = [[UITableView alloc]initWithFrame:self.view.frame];
+        _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, imageHeight+30, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-(imageHeight+30))];
         [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
         _tableView.delegate = self;
         _tableView.dataSource = self;
@@ -27,6 +53,8 @@
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.title = @"个人主页";
     [self.view addSubview:self.tableView];
+    [self.view addSubview:self.headerView];
+    [self.view addSubview:self.toolView];
     //1.凡是在导航条下面的scrollview,默认会设置偏移
     NSLog(@"%@",NSStringFromCGRect(self.tableView.frame));
     //设置不需要默认偏移量
@@ -42,7 +70,39 @@
     
     
 //    self.tableView.contentInset = UIEdgeInsetsMake(100, 0, 0, 0);
+    NSLog(@"%f",self.view.frame.size.width);
+    NSLog(@"%f",[UIScreen mainScreen].bounds.size.width);
+//    self.tableView.translatesAutoresizingMaskIntoConstraints = YES;
+
+//    self.tableView.tableHeaderView = imageView;
+//    self.tableView.autoresizingMask = NO;
+    
 }
+//监听滚动
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    //求偏移量=当前点-最原始的点
+   
+    self.offet = scrollView.contentOffset.y;
+    CGFloat h = imageHeight-self.offet;
+//    [[self.headerView.heightAnchor constraintEqualToConstant:h] setActive:YES];
+//    [[self.headerView.heightAnchor constraintEqualToConstant:40] setActive:true];
+    if(h<91){
+        h = 91;
+    }
+    CGRect rect = self.headerView.frame;
+    rect.size.height = h;
+    NSLog(@"%f",h);
+    self.headerView.frame = rect;
+    CGRect rect2 = self.toolView.frame ;
+    rect2.origin.y = h;
+    self.toolView.frame = rect2;
+    CGRect rect3 = self.tableView.frame ;
+    rect3.origin.y = h+30;
+    rect3.size.height = [UIScreen mainScreen].bounds.size.height-(h+30);
+    self.tableView.frame = rect3;
+}
+
 
 - (void)viewDidAppear:(BOOL)animated
 {
@@ -58,7 +118,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return 100;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
