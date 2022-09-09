5a019f730e54fd66bb90b9d1d22febe7234d1a21
lsaac <2423860442@qq.com>
2022-09-09 22:35:38 +0800

    hittest底层自己实现

--- OC_Learning/ui/UIView拖拽/greenView.m
+++ OC_Learning/ui/UIView拖拽/greenView.m
@@ -9,10 +9,38 @@
 
 @implementation greenView
 
+//自己实现
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
 {
-    NSLog(@"%s",__func__);
-    return [super hitTest:point withEvent:event];
+   /*
+    1.判断自己能否接收事件
+    2.判断当前点在不在当前view
+    3.从后往前遍历自己的子控件，让子控件重复前两步操作(把事件传递给子控件)
+    */
+    //1
+    if(self.userInteractionEnabled == NO || self.isHidden == YES ||self.alpha<=0.01){
+        return nil;
+    }
+    //2
+    if(![self pointInside:point withEvent:event]){
+        return nil;
+    }
+    //3
+    int count = (int)self.subviews.count;
+    for (int i = count-1; i>=0; i--) {
+        //取出子控件
+        UIView *childV =  self.subviews[i];
+        //把当前的点转换成子控件坐标上的点
+        CGPoint childPoint =  [self convertPoint:point toView:childV];
+       UIView *fitView =   [childV hitTest:childPoint withEvent:event];
+        //判断有没有找到最适合的view
+        if(fitView){
+            return fitView;
+        }
+        
+    }
+    //没有找到比它更合适的view了， 那么它自己就是最适合的
+    return self;
 }
 
 @end
