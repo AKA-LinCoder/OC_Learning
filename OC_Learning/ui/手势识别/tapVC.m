cd254cad456992482f41f190ffc571725afd6a01
lsaac <2423860442@qq.com>
2022-09-10 16:29:00 +0800

    手势识别，旋转，缩放，轻扫

--- OC_Learning/ui/手势识别/tapVC.m
+++ OC_Learning/ui/手势识别/tapVC.m
@@ -17,7 +17,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //创建手势
-    [self setUpLongSwipe];
+    [self setUpPinch];
+}
+//是否允许同时接收多个手势
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    return YES;
+}
+
+
+//旋转手势
+-(void) setUpRotaion
+{
+    UIRotationGestureRecognizer *tap = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotaions:)];
+    self.imageView.userInteractionEnabled = YES;
+    //要想同时支持多个手势，必须用代理
+    tap.delegate = self;
+    //添加手势
+    [self.imageView addGestureRecognizer:tap];
+}
+//缩放手势
+-(void) setUpPinch
+{
+    UIPinchGestureRecognizer *tap = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinch:)];
+    self.imageView.userInteractionEnabled = YES;
+    //添加手势
+    [self.imageView addGestureRecognizer:tap];
+}
+
+
+//拖动手势
+-(void) setUpPan
+{
+    /// 当长按时调用(当长按时，该方法会持续调用)
+    UIPanGestureRecognizer *tap = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+    //设置轻扫手势方法，一个轻扫手势只能对应一个手势
+//    tap.direction =
+    self.imageView.userInteractionEnabled = YES;
+    //添加手势
+    [self.imageView addGestureRecognizer:tap];
 }
 
 //轻扫手势
@@ -76,6 +114,34 @@
 -(void) swipe
 {
     NSLog(@"%s",__func__);
+}
+
+-(void) pan:(UIPanGestureRecognizer *)pann
+{
+    NSLog(@"%s",__func__);
+    //获取相对于最原始的偏移量
+    CGPoint transP =  [pann translationInView:self.imageView];
+    //如果直接使用，会导致跳动距离太大
+    self.imageView .transform = CGAffineTransformTranslate(self.imageView.transform, transP.x, transP.y);
+    //让它相对于上一次，复位
+    [pann setTranslation:CGPointZero inView:self.imageView];
+}
+//缩放时调用
+-(void) pinch:(UIPinchGestureRecognizer *)pinch
+{
+    NSLog(@"%s",__func__);
+    self.imageView.transform = CGAffineTransformScale(self.imageView.transform, pinch.scale, pinch.scale);
+    //复位
+    [pinch setScale:1];
+}
+
+//旋转时调用
+-(void) rotations:(UIRotationGestureRecognizer *)rotationGes
+{
+    NSLog(@"%s",__func__);
+    self.imageView.transform = CGAffineTransformRotate(self.imageView.transform, rotationGes.rotation);
+    //复位
+    [rotationGes setRotation:0];
 }
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch
