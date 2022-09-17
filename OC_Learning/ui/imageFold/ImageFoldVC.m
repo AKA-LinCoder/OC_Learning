8149e7c8b45f795e94d55f6f91306fe5db8919e6
lin <2423860442@qq.com>
2022-09-17 14:42:21 +0800

    图片旋转

--- OC_Learning/ui/imageFold/ImageFoldVC.m
+++ OC_Learning/ui/imageFold/ImageFoldVC.m
@@ -20,27 +20,34 @@
     self.view.backgroundColor = [UIColor whiteColor];
     //让上面图片只显示上面，下面图片只显示下面
     self.topImageV.layer.contentsRect = CGRectMake(0, 0, 1, 0.5);
-    
     self.bottomImageV.layer.contentsRect = CGRectMake(0, 0.5, 1, 0.5);
     self.topImageV.layer.anchorPoint = CGPointMake(0.5, 1);
     self.bottomImageV.layer.anchorPoint = CGPointMake(0.5, 0);
 
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)pann:(UIPanGestureRecognizer *)pan {
+    NSLog(@"%s",__func__);
+    //获取移动的偏移量
+    CGPoint transP =  [pan translationInView:pan.view];
+    //上图旋转角度
+    CGFloat angle = transP.y*M_PI /200.0;
+    
+    //近大远小效果
+    CATransform3D trans = CATransform3DIdentity;
+    //眼睛离屏幕距离
+    trans.m34 = -1/500.0;
+    self.topImageV.layer.transform = CATransform3DRotate(trans, -angle, 1, 0, 0);
+    
+    
+    
+    
+//    [UIView animateWithDuration:0.5 animations:^{
+//        self.topImageV.layer.transform = CATransform3DRotate(self.topImageV.layer.transform, M_PI, 1, 0, 0);
+//    }];
 }
-*/
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    [UIView animateWithDuration:0.5 animations:^{
-        self.topImageV.layer.transform = CATransform3DRotate(self.topImageV.layer.transform, M_PI, 1, 0, 0);
-    }];
-}
+
+
 
 @end
